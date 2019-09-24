716efa612b80ff6b469bc08c6789f10988ceae65
Alan Casas Arevalo <acasasar@everis.com>
2019-09-24 11:14:52 +0200

    ==

--- Share/MainViewController.swift
+++ Share/MainViewController.swift
@@ -7,16 +7,62 @@
 //
 
 import UIKit
+import RxSwift
 
 class MainViewController: UIViewController {
+
+    let stream = PublishSubject<Double>()
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let heavyCalc = stream
+            .do(onNext: { _ in
+                print("heavy calc")
+            })
+            .map {
+                $0 * .pi / 180
+            }
+        
+        heavyCalc
+            .map {
+                "\($0)"
+            }
+            .subscribe(onNext: { rad in
+                print("subscribe #1: \(rad)")
+            })
+            .disposed(by: disposeBag)
+        
+        heavyCalc
+            .map {
+                Int($0)
+            }
+            .map {
+                "\($0)"
+            }
+            .subscribe(onNext: { rad in
+                print("subscribe #2: \(rad)")
+            })
+            .disposed(by: disposeBag)
+        
+        heavyCalc
+            .map {
+                "\($0)"
+            }
+            .subscribe(onNext: { rad in
+                print("-----------------")
+            })
+            .disposed(by: disposeBag)
 
     }
 
 
     @IBAction func buttonPressed(_ sender: Any) {
+        
+        let randomDegree = (0...360).randomElement() ?? 0
+        stream.onNext(Double(randomDegree))
+        
     }
     
 }
